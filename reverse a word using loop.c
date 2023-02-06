2b105f06105a8e999b665d3ef32ee685e182047b
vinay9100 <122264277+vinay9100@users.noreply.github.com>
2023-02-05 23:19:29 -0600

    Create reverse a word using loop.c

---
+++ reverse a word using loop.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	int i;
+char str[50];
+printf("enter a string");
+scanf("%s",str);
+ printf("Reverse the String:");
+ for(i=(strlen(str)-1);i>=0;i--)
+ { printf("%c",str[i]);
+ } return 0;
+}
