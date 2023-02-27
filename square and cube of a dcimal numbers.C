af0d8c0ae92ca4ca5eeecdd1db3928934e581b43
vinay9100 <122264277+vinay9100@users.noreply.github.com>
2023-02-27 00:28:02 -0600

    Create square and cube of a dcimal numbers.C

---
+++ square and cube of a dcimal numbers.C
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{float a,b,c;
+printf("enter a number:");
+scanf("%f",&a);
+b=a*a;
+c=a*a*a;
+printf("\n%f is the square of the number",b);
+printf("\n%f is the cube of the number",c);
+return 0;
+}
