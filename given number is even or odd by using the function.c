3825e34602998f94adc29e798226603f7d240b35
vinay9100 <122264277+vinay9100@users.noreply.github.com>
2023-02-27 22:11:54 -0600

    Create given number is even or odd by using the function.c

---
+++ given number is even or odd by using the function.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int isEven(int num) {
+    if (num % 2 == 0) {
+        return 1;
+    } else {
+        return 0; 
+    }
+}
+int main() {
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if (isEven(num)) {
+        printf("%d is even.", num);
+    } else {
+        printf("%d is odd.", num);
+    }
+
+    return 0;
+}
