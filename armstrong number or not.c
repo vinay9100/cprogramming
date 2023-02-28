76fe00abd8e2de81fd145be36f2ee51433dad11f
vinay9100 <122264277+vinay9100@users.noreply.github.com>
2023-02-27 23:06:02 -0600

    Create armstrong number or not.c

---
+++ armstrong number or not.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <math.h>
+int main() {
+    int num, originalNum, remainder, result = 0, n = 0;
+    printf("Enter a positive integer: ");
+    scanf("%d", &num);
+    originalNum = num;
+    while (originalNum != 0) {
+        originalNum /= 10;
+        ++n;
+    }
+    originalNum = num;
+
+    while (originalNum != 0) {
+        remainder = originalNum % 10;
+        result += pow(remainder, n);
+        originalNum /= 10;
+    }
+    if (result == num)
+        printf("%d is an Armstrong number.", num);
+    else
+        printf("%d is not an Armstrong number.", num);
+
+    return 0;
+}
